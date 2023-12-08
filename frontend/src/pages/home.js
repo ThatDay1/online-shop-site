7ed3a4fe181b02ef2fd3f9a9f78400ef280d110a
Ogun Karaoglu <ogunkaraoglu62@gmail.com>
2023-12-08 21:20:07 +0300

    showing images in the main page

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,7 +1,40 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
 const HomeComponent = () => {
+    const [products, setProducts] = useState([]);
+
+    const getAll = async () => {
+        var response = await axios.get("http//localhost:5000/products");
+        setProducts(response.data);
+    }
+
+    useEffect(() => {
+        getAll();
+    }, [])
     return (
         <>
-            <h1>Home Component</h1>
+            <div className='container'>
+                <div className='row'>
+                    {
+                        products.map((product, index) => (
+                            <div key={index} className='col-md-3'>
+                                <div className='card'>
+                                    <div className='card-header'>
+                                        <h4>{product.name}</h4>
+                                    </div>
+                                    <div className='card-body'>
+                                        <img style={{width: "180px", height: "150px"}} src={'http://localhost:5000/' + product.imageUrl}/>
+                                        <h4 className='text-center mt-1' style={{border: "1px solid #ccc", padding:"10px"}}>Stock: {product.stock}</h4>
+                                        <h4 className='text-center text-danger mt-1' style={{border: "1px solid #ccc", padding:"10px"}}>Price: {product.price}</h4>
+                                        <button className='btn btn-outline-success w-100'>Add to Cart</button>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
         </>
     )
 }
